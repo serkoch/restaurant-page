8fdff3279eef43a66c18564551364e9419e88208
nevermind <nevermind359@gmail.com>
2024-12-24 13:43:33 -0500

    Add contact tab content

---
+++ src/contact.js
@@ -0,0 +1,26 @@
+function createContact() {
+  const contact = document.createElement('div');
+  contact.classList.add('contact');
+
+  const phoneNum = document.createElement('p');
+  phoneNum.textContent = '123 456 7890';
+
+  const address = document.createElement('p');
+  address.textContent = 'Manhattan, NY, USA'
+
+  const restaurantMap = document.createElement('img');
+  restaurantMap.src = 'img/map.jpg';
+  restaurantMap.alt = 'Pizzeria Mangione location';
+
+  contact.appendChild(phoneNum);
+  contact.appendChild(address);
+  contact.appendChild(restaurantMap);
+
+  return contact
+}
+
+export default function loadContact() {
+  const main = document.querySelector('#main');
+  main.textContent = '';
+  main.appendChild(createContact());
+}
